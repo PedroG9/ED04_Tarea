c5a4c56431152a9d4a8024a273620f08bf2954f9
PedroG9 <pedrogarciatalavera1997@hotmail.com>
2025-03-07 21:17:02 +0100

    Insertar comentarios JavaDoc

--- src/entidadFinanciera/CCuenta.java
+++ src/entidadFinanciera/CCuenta.java
@@ -1,81 +1,98 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package entidadFinanciera;
 
+/*
+ * Clase que reprensenta una cuenta bancaria
+ * Se guarda el nombre del titular, el número de cuenta, el saldo y el tipo de interés
+ */
 
 public class CCuenta {
 
-    /**
-     * @return the nombre
+    /*
+     * Devuelve el nombre del titular
+     * @return nombre del titular
      */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
+    /*
+     * Establece el nombre del titular
+     * @param  nombre a poner en la cuenta
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
-    /**
-     * @return the cuenta
+    /*
+     * Devuelve número de cuenta
+     * @return número de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
-    /**
-     * @param cuenta the cuenta to set
+    /*
+     * Establece número de cuenta
+     * @param número de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-    /**
+    /*
+     * Devuelve el saldo de la cuenta
      * @return the saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
-    /**
+    /*
+     * Establece el saldo de la cuenta
      * @param saldo the saldo to set
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-    /**
-     * @return the tipoInterés
+    /*
+     * Devuelve el tipo de interés
+     * @return el tipo de interés
      */
     public double getTipoInterés() {
         return tipoInterés;
     }
 
-    /**
-     * @param tipoInterés the tipoInterés to set
+    /*
+     * Establece el tipo de interés
+     * @param el tipo de interés
      */
     public void setTipoInterés(double tipoInterés) {
         this.tipoInterés = tipoInterés;
     }
 
-    /**
-     * Nombre de la persona titular
+    /*
+     * Se establecen las variables
      */
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /*
+     * Constructor vacío para crear una cuenta sin valores al inicio
+     */
     public CCuenta()
     {
     }
 
+    /*
+     * Constructor que crea una cuenta
+     * @param nom Nombre del titular
+     * @param cue Número de la cuenta
+     * @param sal Saldo inicial
+     * @param tipo Tipo de interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -83,10 +100,21 @@ public class CCuenta {
         saldo=sal;
     }
     
+    /*
+     * Devuelve el saldo actual
+     * @return saldo
+     */
+    
     public double estado(){
         return this.getSaldo();
     }
 
+    /*
+     * Ingresa una cantidad de dinero
+     * @param cantidad a ingresar
+     * @throws Exception Se lanza una excepción si se ingresa una cantidad negativa
+     */
+    
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -94,6 +122,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /*
+     * Retira una cantidad de dinero
+     * @param cantidad que retira
+     * @throws Exception Lanza una excepción si no hay saldo o la cantidad es negativa
+     */
+    
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -102,6 +136,10 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
+    
+    /*
+     * Mensaje de error cuando se intenta retirar una cantidad negativa
+     */
     private static final String ERRORCANTIDADNEGATIVA = "No se puede retirar una cantidad negativa";
 }
 
--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -1,43 +1,73 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package entidadFinanciera;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/*
+ * Clase principal de la aplicación
+ */
 
 public class Main {
 
     public static void main(String[] args) {
+        
+        // Crea un objeto de la clase CCuenta
+        
         CCuenta objetoCuenta;
         double saldoActual;
         
-
+        // Inicializar el objetoCuenta con valores: nombre, cuenta, saldo y tipoInteres
+        
         objetoCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        
+        // Obtiene el saldo usando el método estado() de CCuenta
+        
         saldoActual = objetoCuenta.estado();
+        
+        //Muestra el saldo actual por consola
+        
         System.out.println("El saldo actual es"+ saldoActual );
         
+        // Prueba la función ingresar()
         probarIngresar(objetoCuenta, saldoActual, null);
         
+        // Prueba la función retirar()
         probarRetirar(objetoCuenta, saldoActual, null);
     
     }
 
+    /*
+     * Función para probar el método retirar de CCuenta
+     * @param objetoCuenta Objeto cuenta en el que se prueba
+     * @param saldoActual Saldo de la cuenta
+     * @param cantidadEsperada No se utiliza
+     */
+    
     private static void probarRetirar(CCuenta objetoCuenta, double saldoActual, Object cantidadEsperada) {
         try {
+            // Se intenta retirar el saldo actual
             objetoCuenta.retirar(saldoActual);
+            
         } catch (Exception ex) {
+            // Se registra un error si ocurre la excepción
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+    /*
+     * Función para probar el método ingresar de CCuenta
+     * @param objetoCuenta Objeto cuenta en el que se prueba
+     * @param saldoActual Salfo de la cuenta
+     * @param cantidadEsperada No se utiliza
+     */
+    
     private static void probarIngresar(CCuenta objetoCuenta, double saldoActual, Object cantidadEsperada) {
         try {
+            // Se intenta ingresar el saldo actual
             objetoCuenta.ingresar(saldoActual);
+            
         } catch (Exception ex) {
+            //Se registra un error si ocurre la excepción
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
